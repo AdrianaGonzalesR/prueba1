76655c9f8896a9016a325136c6ec635f8b5d8ada
Kevin Candia <kevin_6_14@hotmail.com>
2024-03-02 22:19:51 -0500

    Initial Commit

---
+++ prueba1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prueba1
+//
+//  Created by Kevin Candia Villagómez on 2/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
